ae82996c4219de1ddfaf893a4d3e28cae07788a1
Richard Dymond <rjdymond@gmail.com>
2014-07-25 18:10:15 -0300

    Add tests

--- Makefile
+++ Makefile
@@ -2,6 +2,10 @@ BUILD = build
 SKOOLKIT = $(BUILD)/skoolkit
 HTML_OPTIONS = $(HTML_OPTS)
 HTML_OPTIONS += -d ../html -t
+NOSETESTS27 = /usr/bin/python2.7 /usr/bin/nosetests
+NOSETESTS32 = /usr/bin/python3.2 /usr/bin/nosetests
+NOSETESTS33 = $(HOME)/Python/Python3.3/bin/nosetests
+NOSETESTS34 = $(HOME)/Python/Python3.4/bin/nosetests
 
 HTML_OPTIONS += $(foreach theme,$(THEMES),-T $(theme))
 ifeq ($(findstring spectrum,$(THEMES)),spectrum)
@@ -13,6 +17,11 @@ usage:
 	@echo "Supported targets:"
 	@echo "  usage     show this help"
 	@echo "  jsw       build the Jet Set Willy disassembly"
+	@echo "  test      run tests with current Python interpreter"
+	@echo "  test2.7   run tests with Python 2.7"
+	@echo "  test3.2   run tests with Python 3.2"
+	@echo "  test3.3   run tests with Python 3.3"
+	@echo "  test3.4   run tests with Python 3.4"
 	@echo "  snapshot  build a snapshot of Jet Set Willy"
 	@echo ""
 	@echo "Environment variables:"
@@ -55,3 +64,27 @@ jet_set_willy.z80: skoolkit-home
 
 .PHONY: snapshot
 snapshot: jet_set_willy.z80
+
+.PHONY: write-tests
+write-tests:
+	for t in asm ctl html sft; do utils/write-tests.py $$t > tests/test_$$t.py; done
+
+.PHONY: test
+test: write-tests
+	nosetests -w tests
+
+.PHONY: test2.7
+test2.7: write-tests
+	$(NOSETESTS27) -w tests
+
+.PHONY: test3.2
+test3.2: write-tests
+	$(NOSETESTS32) -w tests
+
+.PHONY: test3.3
+test3.3: write-tests
+	$(NOSETESTS33) -w tests
+
+.PHONY: test3.4
+test3.4: write-tests
+	$(NOSETESTS34) -w tests
---
+++ tests/jswtest.py
@@ -0,0 +1,290 @@
+# -*- coding: utf-8 -*-
+import sys
+PY3 = sys.version_info >= (3,)
+if PY3:
+    from io import StringIO
+else:
+    from StringIO import StringIO
+import os
+from os.path import dirname
+import shutil
+from subprocess import Popen, PIPE
+import tempfile
+from lxml import etree
+from xml.dom.minidom import parse
+from xml.dom import Node
+from unittest import TestCase
+
+# Use the current development version of SkoolKit
+SKOOLKIT_HOME = os.environ.get('SKOOLKIT_HOME')
+if not SKOOLKIT_HOME:
+    sys.stderr.write('SKOOLKIT_HOME is not set; aborting\n')
+    sys.exit(1)
+if not os.path.isdir(SKOOLKIT_HOME):
+    sys.stderr.write('SKOOLKIT_HOME={}: directory not found\n'.format(SKOOLKIT_HOME))
+    sys.exit(1)
+
+JSW2CTL = '../utils/jsw2ctl.py'
+JSWZ80 = '../jet_set_willy.z80'
+JSWREF = '../jet_set_willy.ref'
+JSWMOD = '../skoolkit/jetsetwilly.py'
+
+XHTML_XSD = os.path.join(SKOOLKIT_HOME, 'XSD', 'xhtml1-strict.xsd')
+
+OUTPUT_JSW = """Creating directory {odir}
+Using skool file: {skoolfile}
+Using ref file: {reffile}
+Parsing {skoolfile}
+Creating directory {odir}/jet_set_willy
+Copying {cssfile} to {odir}/jet_set_willy/{cssfile}
+  Writing disassembly files in jet_set_willy/asm
+  Writing jet_set_willy/maps/all.html
+  Writing jet_set_willy/maps/routines.html
+  Writing jet_set_willy/maps/data.html
+  Writing jet_set_willy/maps/messages.html
+  Writing jet_set_willy/maps/unused.html
+  Writing jet_set_willy/buffers/gbuffer.html
+  Writing jet_set_willy/reference/changelog.html
+  Writing jet_set_willy/reference/bugs.html
+  Writing jet_set_willy/reference/facts.html
+  Writing jet_set_willy/reference/pokes.html
+  Writing jet_set_willy/index.html"""
+
+def _find_ids_and_hrefs(elements, doc_anchors, doc_hrefs):
+    for node in elements:
+        if node.nodeType == Node.ELEMENT_NODE:
+            element_id = node.getAttribute('id')
+            if element_id:
+                doc_anchors.add(element_id)
+            if node.tagName in ('a', 'link', 'img', 'script'):
+                if node.tagName == 'a':
+                    element_name = node.getAttribute('name')
+                    if element_name:
+                        doc_anchors.add(element_name)
+                if node.tagName in ('a', 'link'):
+                    element_href = node.getAttribute('href')
+                    if element_href:
+                        doc_hrefs.add(element_href)
+                elif node.tagName in ('img', 'script'):
+                    element_src = node.getAttribute('src')
+                    if element_src:
+                        doc_hrefs.add(element_src)
+            _find_ids_and_hrefs(node.childNodes, doc_anchors, doc_hrefs)
+
+def _read_files(root_dir):
+    all_files = {} # filename -> (element ids and <a> names, hrefs and srcs)
+    for root, dirs, files in os.walk(root_dir):
+        for f in files:
+            fname = os.path.join(root, f)
+            all_files[fname] = (set(), set())
+            if f.endswith('.html'):
+                doc = parse(fname)
+                _find_ids_and_hrefs(doc.documentElement.childNodes, *all_files[fname])
+    return all_files
+
+def check_links(root_dir):
+    missing_files = []
+    missing_anchors = []
+    all_files = _read_files(root_dir)
+    linked = set()
+    for fname in all_files:
+        for href in all_files[fname][1]:
+            if not href.startswith('http://'):
+                if href.startswith('#'):
+                    link_dest = fname + href
+                else:
+                    link_dest = os.path.normpath(os.path.join(os.path.dirname(fname), href))
+                dest_fname, sep, anchor = link_dest.partition('#')
+                linked.add(dest_fname)
+                if dest_fname not in all_files:
+                    missing_files.append((fname, link_dest))
+                elif anchor and anchor not in all_files[dest_fname][0]:
+                    missing_anchors.append((fname, link_dest))
+    orphans = set()
+    for fname in all_files:
+        if fname not in linked:
+            orphans.add(fname)
+    return all_files, orphans, missing_files, missing_anchors
+
+class Stream:
+    def __init__(self):
+        self.buffer = StringIO()
+
+    def write(self, text):
+        self.buffer.write(text)
+
+    def getvalue(self):
+        return self.buffer.getvalue()
+
+    def clear(self):
+        self.buffer.seek(0)
+        self.buffer.truncate()
+
+class DisassembliesTestCase(TestCase):
+    def setUp(self):
+        self.maxDiff = None
+        self.stdout = sys.stdout
+        self.stderr = sys.stderr
+        sys.stdout = self.out = Stream()
+        sys.stderr = self.err = Stream()
+        self.tempfiles = []
+        self.tempdirs = []
+        self._copy_skoolkit()
+
+    def tearDown(self):
+        for f in self.tempfiles:
+            if os.path.isfile(f):
+                os.remove(f)
+        for d in self.tempdirs:
+            if os.path.isdir(d):
+                shutil.rmtree(d, True)
+        sys.stdout = self.stdout
+        sys.stderr = self.stderr
+
+    def _copy_skoolkit(self):
+        tempdir = 'skoolkit-{}'.format(os.getpid())
+        self.tempdirs.append(tempdir)
+        skoolkitdir = os.path.join(tempdir, 'skoolkit')
+        os.makedirs(skoolkitdir)
+
+        skdevdir = os.path.join(SKOOLKIT_HOME, 'skoolkit')
+        for f in os.listdir(skdevdir):
+            if f.endswith('.py'):
+                shutil.copy2(os.path.join(skdevdir, f), skoolkitdir)
+        shutil.copy2(JSWMOD, skoolkitdir)
+
+        if sys.path[0] != tempdir:
+            sys.path.insert(0, tempdir)
+            global skool2asm, skool2ctl, skool2html, skool2sft, sna2skool
+            from skoolkit import skool2asm, skool2ctl, skool2html, skool2sft, sna2skool
+
+    def _to_lines(self, text):
+        # Use rstrip() to remove '\r' characters (useful on Windows)
+        lines = [line.rstrip() for line in text.split('\n')]
+        if lines[-1] == '':
+            lines.pop()
+        return lines
+
+    def write_text_file(self, contents='', suffix=''):
+        fd, path = tempfile.mkstemp(suffix=suffix, dir='', text=True)
+        path = os.path.basename(path.replace(os.path.sep, '/'))
+        f = os.fdopen(fd, 'wt')
+        self.tempfiles.append(path)
+        f.write(contents)
+        f.close()
+        return path
+
+    def run_skoolkit_command(self, cmd, args, out_lines=True, err_lines=False):
+        self.out.clear()
+        self.err.clear()
+        try:
+            cmd(args.split())
+        except SystemExit:
+            pass
+        out = self._to_lines(self.out.getvalue()) if out_lines else self.out.getvalue()
+        err = self._to_lines(self.err.getvalue()) if err_lines else self.err.getvalue()
+        return out, err
+
+    def _run_cmd(self, cmd):
+        cmdline = cmd.split()
+        if sys.platform == 'win32':
+            cmdline.insert(0, 'python')
+        p = Popen(cmdline, stdout=PIPE, stderr=PIPE)
+        out, err = p.communicate()
+        if PY3:
+            out = out.decode()
+            err = err.decode()
+        return out, err
+
+    def write_jsw_skool(self):
+        os.environ['SKOOLKIT_HOME'] = SKOOLKIT_HOME
+        ctl, error = self._run_cmd('{} {}'.format(JSW2CTL, JSWZ80))
+        self.assertEqual(len(error), 0)
+        options = '-c {}'.format(self.write_text_file(ctl))
+        skool, error = self.run_skoolkit_command(sna2skool.main, '{} {}'.format(options, JSWZ80), out_lines=False)
+        self.assertEqual(len(error), 0)
+        return self.write_text_file(skool)
+
+class AsmTestCase(DisassembliesTestCase):
+    def write_jsw(self, options):
+        skoolfile = self.write_jsw_skool()
+        args = '{} {}'.format(options, skoolfile)
+        output, error = self.run_skoolkit_command(skool2asm.main, args, err_lines=True)
+        self.assertTrue(any([line.startswith('Parsed {}'.format(skoolfile)) for line in error]))
+        self.assertTrue(error[-1].startswith('Wrote ASM to stdout'))
+
+class CtlTestCase(DisassembliesTestCase):
+    def write_jsw(self, options):
+        args = '{} {}'.format(options, self.write_jsw_skool())
+        output, stderr = self.run_skoolkit_command(skool2ctl.main, args)
+        self.assertEqual(stderr, '')
+
+class HtmlTestCase(DisassembliesTestCase):
+    def setUp(self):
+        DisassembliesTestCase.setUp(self)
+        self.odir = 'html-{}'.format(os.getpid())
+        self.tempdirs.append(self.odir)
+
+    def _validate_xhtml(self):
+        if os.path.isfile(XHTML_XSD):
+            with open(XHTML_XSD) as f:
+                xmlschema_doc = etree.parse(f)
+            xmlschema = etree.XMLSchema(xmlschema_doc)
+            for root, dirs, files in os.walk(self.odir):
+                for fname in files:
+                    if fname[-5:] == '.html':
+                        htmlfile = os.path.join(root, fname)
+                        try:
+                            xhtml = etree.parse(htmlfile)
+                        except etree.LxmlError as e:
+                            self.fail('Error while parsing {}: {}'.format(htmlfile, e.message))
+                        try:
+                            xmlschema.assertValid(xhtml)
+                        except etree.DocumentInvalid as e:
+                            self.fail('Error while validating {}: {}'.format(htmlfile, e.message))
+
+    def _check_links(self):
+        all_files, orphans, missing_files, missing_anchors = check_links(self.odir)
+        if orphans or missing_files or missing_anchors:
+            error_msg = []
+            if orphans:
+                error_msg.append('Orphaned files: {}'.format(len(orphans)))
+                for fname in orphans:
+                    error_msg.append('  {}'.format(fname))
+            if missing_files:
+                error_msg.append('Links to non-existent files: {}'.format(len(missing_files)))
+                for fname, link_dest in missing_files:
+                    error_msg.append('  {} -> {}'.format(fname, link_dest))
+            if missing_anchors:
+                error_msg.append('Links to non-existent anchors: {}'.format(len(missing_anchors)))
+                for fname, link_dest in missing_anchors:
+                    error_msg.append('  {} -> {}'.format(fname, link_dest))
+            self.fail('\n'.join(error_msg))
+
+    def write_jsw(self, options):
+        skoolfile = self.write_jsw_skool()
+        cssfile = self.write_text_file(suffix='.css')
+        c_options = '-c Game/StyleSheet={}'.format(cssfile)
+        c_options += ' -c Config/SkoolFile={}'.format(skoolfile)
+        shutil.rmtree(self.odir, True)
+
+        # Write the disassembly
+        output, error = self.run_skoolkit_command(skool2html.main, '{} -d {} {} {}'.format(c_options, self.odir, options, JSWREF))
+        self.assertEqual(len(error), 0)
+        reps = {'odir': self.odir, 'cssfile': cssfile, 'skoolfile': skoolfile, 'reffile': JSWREF}
+        self.assertEqual(OUTPUT_JSW.format(**reps).split('\n'), output)
+
+        self._validate_xhtml()
+        self._check_links()
+
+class SftTestCase(DisassembliesTestCase):
+    def write_jsw(self, options):
+        skoolfile = self.write_jsw_skool()
+        with open(skoolfile, 'rt') as f:
+            orig_skool = f.read().split('\n')
+        args = '{} {}'.format(options, skoolfile)
+        sft, error = self.run_skoolkit_command(skool2sft.main, args, out_lines=False)
+        self.assertEqual(error, '')
+        options = '-T {}'.format(self.write_text_file(sft))
+        skool, error = self.run_skoolkit_command(sna2skool.main, '{} {}'.format(options, JSWZ80))
+        self.assertEqual(orig_skool[:-1], skool)
