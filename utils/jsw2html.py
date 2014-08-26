10e1c779857e0a1eef256c02a1cc35d5cbd90551
Richard Dymond <rjdymond@gmail.com>
2014-08-25 21:04:49 -0300

    Use the '-W' option of skool2html.py in utils/jsw2html.py

--- utils/jsw2html.py
+++ utils/jsw2html.py
@@ -8,6 +8,7 @@ except ImportError:
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 build_dir = '{}/build'.format(parent_dir)
+writer_class = '{}/skoolkit:jetsetwilly.JetSetWillyHtmlWriter'.format(parent_dir)
 
 skool2html_options = '-d {}/html'.format(build_dir)
 SKOOLKIT_HOME = os.environ.get('SKOOLKIT_HOME')
@@ -18,12 +19,14 @@ if SKOOLKIT_HOME:
     sys.path.insert(0, SKOOLKIT_HOME)
     from skoolkit import skool2html, sna2skool, tap2sna
     skool2html_options += ' -S {0}/resources -S {1} -S {1}/resources'.format(SKOOLKIT_HOME, parent_dir)
+    skool2html_options += ' -W {}'.format(writer_class)
 else:
     try:
         from skoolkit import skool2html, sna2skool, tap2sna
     except ImportError:
         sys.stderr.write('Error: SKOOLKIT_HOME is not set, and SkoolKit is not installed\n')
         sys.exit(1)
+    skool2html_options = '-c Config/HtmlWriterClass={}'.format(writer_class)
 
 sys.stderr.write("Found SkoolKit in {}\n".format(skool2html.PACKAGE_DIR))
 
@@ -52,7 +55,5 @@ with open(skoolfile, 'wt') as f:
 sys.stdout = stdout
 
 # Build the HTML disassembly
-writer_class = '{}/skoolkit:jetsetwilly.JetSetWillyHtmlWriter'.format(parent_dir)
-options = '-c Config/HtmlWriterClass={} {}'.format(writer_class, skool2html_options)
-args = options.split() + sys.argv[1:] + [skoolfile]
+args = skool2html_options.split() + sys.argv[1:] + [skoolfile]
 skool2html.main(args)
