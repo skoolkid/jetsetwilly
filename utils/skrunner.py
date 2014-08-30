81bba86f3cf62e570b750c895207cd12255a49ec
Richard Dymond <rjdymond@gmail.com>
2014-08-30 12:24:53 -0300

    Update skrunner.py to work with an installed version of SkoolKit 4.1

--- utils/skrunner.py
+++ utils/skrunner.py
@@ -8,9 +8,7 @@ except ImportError:
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 build_dir = '{}/build'.format(parent_dir)
-writer_class = '{}/skoolkit:jetsetwilly.JetSetWillyHtmlWriter'.format(parent_dir)
 
-skool2html_options = '-d {}/html'.format(build_dir)
 SKOOLKIT_HOME = os.environ.get('SKOOLKIT_HOME')
 if SKOOLKIT_HOME:
     if not os.path.isdir(SKOOLKIT_HOME):
@@ -18,15 +16,12 @@ if SKOOLKIT_HOME:
         sys.exit(1)
     sys.path.insert(0, SKOOLKIT_HOME)
     from skoolkit import skool2asm, skool2html, sna2skool, tap2sna
-    skool2html_options += ' -S {0}/resources -S {1} -S {1}/resources'.format(SKOOLKIT_HOME, parent_dir)
-    skool2html_options += ' -W {}'.format(writer_class)
 else:
     try:
         from skoolkit import skool2asm, skool2html, sna2skool, tap2sna
     except ImportError:
         sys.stderr.write('Error: SKOOLKIT_HOME is not set, and SkoolKit is not installed\n')
         sys.exit(1)
-    skool2html_options = '-c Config/HtmlWriterClass={}'.format(writer_class)
 
 sys.stderr.write("Found SkoolKit in {}\n".format(skool2html.PACKAGE_DIR))
 
@@ -62,5 +57,9 @@ def run_skool2asm():
     skool2asm.main(args)
 
 def run_skool2html():
+    writer_class = '{}/skoolkit:jetsetwilly.JetSetWillyHtmlWriter'.format(parent_dir)
+    skool2html_options = '-d {}/html'.format(build_dir)
+    skool2html_options += ' -S {0}/resources -S {1} -S {1}/resources'.format(SKOOLKIT_HOME, parent_dir)
+    skool2html_options += ' -W {}'.format(writer_class)
     args = skool2html_options.split() + sys.argv[1:] + [write_skool()]
     skool2html.main(args)
