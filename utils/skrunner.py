00abff48112beae3845dd250b3ca54ca5921137e
Richard Dymond <rjdymond@gmail.com>
2016-03-21 18:52:25 -0300

    Remove the '-W' option from run_skool2{asm,html}() in utils/skrunner.py

--- utils/skrunner.py
+++ utils/skrunner.py
@@ -22,10 +22,8 @@ else:
 sys.stderr.write("Found SkoolKit in {}\n".format(skool2html.PACKAGE_DIR))
 
 def run_skool2asm():
-    options = '-W {}/sources:jetsetwilly.JetSetWillyAsmWriter'.format(JETSETWILLY_HOME)
-    skool2asm.main(options.split() + sys.argv[1:] + [JSW_SKOOL])
+    skool2asm.main(sys.argv[1:] + [JSW_SKOOL])
 
 def run_skool2html():
     options = '-d {}/build/html'.format(JETSETWILLY_HOME)
-    options += ' -W {}/sources:jetsetwilly.JetSetWillyHtmlWriter'.format(JETSETWILLY_HOME)
     skool2html.main(options.split() + sys.argv[1:] + [JSW_SKOOL])
