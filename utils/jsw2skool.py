7db603ee22771130dbc8e82420af79e536a91d92
Richard Dymond <rjdymond@gmail.com>
2017-01-10 18:26:29 -0400

    Add @replace directives for the #BUG, #FACT and #POKE macros

--- utils/jsw2skool.py
+++ utils/jsw2skool.py
@@ -385,7 +385,7 @@ class JetSetWilly:
             while b < a + 205 and self.snapshot[b] != conveyor_attr:
                 b += 1
             if b < a + 205:
-                comment += ' Note that because of a #BUG#corruptedConveyors in the game engine, the conveyor tile is not drawn correctly (see the room image above).'
+                comment += ' Note that because of a #BUG#corruptedConveyors(bug) in the game engine, the conveyor tile is not drawn correctly (see the room image above).'
         lines.append('N {} {}'.format(a + 160, comment))
         lines.append('N {} {}'.format(a + 160, tiles_table))
         if a == 57088:
